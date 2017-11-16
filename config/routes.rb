600ad187a609dd8f7379e97eaad74412bae1e938
Courtney VanCaeyzeele <cvancaeyzeele@gmail.com>
2017-11-15 21:27:12 -0600

    Created views and routes for About and Contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'pages/contact', to: 'pages#contact', as: 'contact'
+
+  get 'pages/about', to: 'pages#about', as: 'about'
+
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
